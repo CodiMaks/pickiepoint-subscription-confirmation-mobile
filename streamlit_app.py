3352598e94f3c993966a929651b84638d1b29635
CodiMaks <150930184+CodiMaks@users.noreply.github.com>
2024-08-02 00:49:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,10 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
-st.caption('<span style="font-size:30px; font-weight:bold; color:white;">Thanks for your subscription</span>', unsafe_allow_html=True)
-st.text("")
+st.caption('<span style="font-size:40px; font-weight:bold; color:white;">Thanks for your subscription</span>', unsafe_allow_html=True)
+st.write("")
 
-st.image("tick.png", width=270)
+st.image("tick.png", width=260)
 st.write("")
 
 st.subheader("You're all set")
@@ -22,4 +22,5 @@ st.subheader("Start picking the main points")
 st.subheader("")
 
 return_to_website = st.button("Let's go", type="primary", use_container_width=True)
+st.text("")
 st.image("pickiepoint_logo_profile_picture.png", use_column_width=True)
