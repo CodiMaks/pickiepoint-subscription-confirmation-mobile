d996f5917be4cebfe035243caf66b5c0eb2d18d4
CodiMaks <150930184+CodiMaks@users.noreply.github.com>
2024-08-02 00:52:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,13 +11,13 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
-st.caption('<span style="font-size:40px; font-weight:bold; color:white;">\u00A0\u00A0\u00A0Thanks for your \u00A0\u00A0 subscription</span>', unsafe_allow_html=True)
+st.caption('<span style="font-size:40px; font-weight:bold; color:white;">Thanks for your \u00A0\u00A0 subscription</span>', unsafe_allow_html=True)
 st.write("")
 
 st.image("tick.png", width=265)
 st.write("")
 
-st.header("You're all set")
+st.header("\u00A0\u00A0\u00A0You're all set")
 st.header("Start picking the main points")
 st.subheader("")
 
