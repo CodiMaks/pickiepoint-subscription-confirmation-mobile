23a963a934c0aaac93645177fb290d521bc8d1a6
CodiMaks <150930184+CodiMaks@users.noreply.github.com>
2024-08-01 17:06:04 +0200

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config("Subscription confirmation", "pickiepoint_logo_profile_picture.png")
+
+st.title("Thanks for subscribing to Pickiepoint")
+st.image("tick.png")
+st.subheader("You're all set")
+st.subheader("You can return to the website")
+st.subheader("Start picking the main points")
+st.image("pickiepoint_logo_profile_picture.png")
