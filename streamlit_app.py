b9eb2abd8f4d16ad58a30f25025984d0b27cdc34
CodiMaks <150930184+CodiMaks@users.noreply.github.com>
2024-08-02 00:55:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,8 @@ st.write("")
 st.image("tick.png", width=265)
 st.write("")
 
-st.caption('<span style="font-size:28px; font-weight:bold; color:white;">You are all set</span>', unsafe_allow_html=True)
-st.caption('<span style="font-size:28px; font-weight:bold; color:white;">Start picking the main points</span>', unsafe_allow_html=True)
+st.caption('<span style="font-size:25px; font-weight:bold; color:white;">\u00A0\u00A0\u00A0\u00A0You are all set</span>', unsafe_allow_html=True)
+st.caption('<span style="font-size:25px; font-weight:bold; color:white;">Start picking the main points</span>', unsafe_allow_html=True)
 st.subheader("")
 
 return_to_website = st.button("Let's go", type="primary", use_container_width=True)
