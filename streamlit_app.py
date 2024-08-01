b65a788f00aa0b46700f072abb7d2fdb1a341df5
CodiMaks <150930184+CodiMaks@users.noreply.github.com>
2024-08-02 00:44:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,13 +11,12 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
-col_1, col_2, col_3 = st.columns(3)
-with col_2:
-    st.title("Thanks for your subscription to Pickiepoint")
-st.subheader("")
+st.title("Thanks for your subscription")
+st.text("")
 
 st.image("tick.png", width=270)
 st.subheader("You're all set")
 st.subheader("You can return to the website")
 st.subheader("Start picking the main points")
+return_to_website = st.button("Let's go")
 st.image("pickiepoint_logo_profile_picture.png", use_column_width=True)
