4e6ced0ff0ea7f8c95413d9be94b30ef69913cb5
CodiMaks <150930184+CodiMaks@users.noreply.github.com>
2024-08-02 00:50:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
-st.caption('<span style="font-size:40px; font-weight:bold; color:white;">Thanks for your     subscription</span>', unsafe_allow_html=True)
+st.caption('<span style="font-size:40px; font-weight:bold; color:white;">Thanks for your \u00A0\u00A0\u00A0 subscription</span>', unsafe_allow_html=True)
 st.write("")
 
 st.image("tick.png", width=265)
